ef036b26fc1a384b97be2a3c4efbdfaacdc17d8d
Rakesh709 <kumar.rakesh1997@outlook.com>
2024-05-22 23:17:21 +0530

    Big O Part2

---
+++ section2-classes_and_pointers/clasess.js
@@ -0,0 +1,14 @@
+// class Cookie{
+// 	constructor(color){
+// 		this.color=color
+// 	};
+// };
+
+// let cookieOne = new Cookie("green")
+
+
+class Cookie{
+	constructor(color){
+		this.color=color
+	};
+};
