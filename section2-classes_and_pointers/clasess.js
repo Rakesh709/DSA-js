8b677c547f7c4ab0c223484071ddc4b639efd39a
Rakesh709 <kumar.rakesh1997@outlook.com>
2024-05-22 23:52:33 +0530

    classes intro

--- section2-classes_and_pointers/clasess.js
+++ section2-classes_and_pointers/clasess.js
@@ -11,4 +11,34 @@ class Cookie{
 	constructor(color){
 		this.color=color
 	};
+
+	//getter 
+	getColor(){
+		return this.color
+	}
+
+	//setter
+	// setColor(color){
+	// 	this.color=color
+	// }
+
+	setColor(color){
+		this.color= color
+		return this.color;
+	}
+
+
 };
+
+let CookieOne = new Cookie("green")
+
+let getcolour=CookieOne.getColor()
+// CookieOne.setColor("yellow") // No need to assign this to newColor
+let newColour = CookieOne.setColor("purple");
+
+// Outputs "yellow"
+
+console.log(getcolour) // Outputs "green"
+// console.log(CookieOne.getColor())  // Outputs "yellow"
+
+console.log(newColour)
