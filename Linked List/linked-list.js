b5829ed91a427be33291aba4b0146c7f25d94e9d
Rakesh709 <kumar.rakesh1997@outlook.com>
2024-05-25 18:32:20 +0530

    Linked List constructor

---
+++ Linked List/linked-list.js
@@ -0,0 +1,64 @@
+// what is f=going on uder the hood of ll.
+
+// {
+// 	head:{
+// 		value:11,
+// 		next:{
+// 			value : 3,
+// 			next :{
+// 				value: 27,
+// 				next: {
+// 					value :4,
+//     tail:---------->next :null
+// 				}
+// 			}
+// 		}
+// 	}
+// }
+
+//------------------Linked List Constructor---------------------------------
+class Node{
+	constructor(value){
+		this.value= value
+		this.next= null
+	}
+}
+
+
+
+class LinkedList{
+	constructor(value){
+		//create a new node
+		const newNode = new Node(value)
+		this.head = newNode
+		this.tail = this.head
+		this.length = 1
+	}
+	push(value){
+		//create a new node
+		//add node to end
+	}
+	unshift(value){
+		//create new node
+		//add node to biginning
+	}
+	inser(value){
+		//create new node
+		//insert node
+	}
+}
+
+// create a new node is common, so that we can write new class 
+//value:11,
+// 		next:
+
+
+
+
+//to create a new node  below method 
+
+// const newNode = new Node(4) 
+
+let myLinkedList = new LinkedList(4)
+
+console.log(myLinkedList)
